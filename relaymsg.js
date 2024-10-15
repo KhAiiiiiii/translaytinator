4927a7ac1dcff3ccbcec418f1685c02d7b7c14da
hubbingingit <120624468+harshinimm@users.noreply.github.com>
2024-10-15 19:02:50 +0800

    Update relaymsg.js

--- relaymsg.js
+++ relaymsg.js
@@ -1,8 +1,6 @@
 const axios = require('axios');
-
 const apiKey = '';
 const modelId = 'jusshini/genz_slang_model';
-
 async function translateSlang(text) {
     const response = await axios.post(
         `https://api-inference.huggingface.co/models/${modelId}`,
