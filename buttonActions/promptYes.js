fe8a5c0c0a9d3fd06bdb2eb6908085a7259915ce
Kyle Cheong <cheongruohkai@gmail.com>
2024-10-20 13:16:57 +0800

    feat: Add translate commands

--- buttonActions/promptYes.js
+++ buttonActions/promptYes.js
@@ -2,6 +2,8 @@ const { ActionRowBuilder, ButtonBuilder } = require('discord.js')
 
 const { translateSlang } = require('../oracle/llama.js')
 
+const { translationResultEmbedBuilder } = require('../embeds/index.js')
+
 module.exports = async ({ inter }) => {
     await inter.message.edit({
         components: inter.message.components.map(
@@ -23,14 +25,15 @@ module.exports = async ({ inter }) => {
     const { channelId, messageId } = inter.message.reference
     const originalMessage = inter.guild.channels.cache.get(channelId)?.messages.cache.get(messageId)
     if(!originalMessage) return inter.editReply('Error parsing original message!')
-        
+    await inter.channel.sendTyping()
+
     const prompt = client.config.translationBasePrompt
                     .replace('{0}', originalMessage.content)
-                    .replace('{1}', 'Gen Z slang')
-                    .replace('{2}', 'normal English')
+                    .replace('{1}', 'normal English')
     const translatedMessage = await translateSlang(prompt)
+    const translationEmbed = translationResultEmbedBuilder({ translatedText: translatedMessage, originalMessage: originalMessage.url })
 
-    originalMessage.reply(translatedMessage)
+    originalMessage.reply({ embeds: [ translationEmbed ] })
 
     loaded = true
     await inter.message.delete()
--- config.js
+++ config.js
@@ -3,7 +3,8 @@ module.exports = {
         type: 'Custom',
         status: 'Translating your messages :)'
     },
+    aiModel: 'Meta Llama 3 8B',
     datasetUpdateInterval: 86400e3, // update dataset every day
     promptThreshold: 0.4, // treshold of slang detected in a message when bot prompts for translation
-    translationBasePrompt: "{0}\nHelp me translate the text above from {1} to {2}",
+    translationBasePrompt: "{0}\nHelp me translate the text above to {1}",
 }
